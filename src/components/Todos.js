a34e657d72d11a6c7862b34c5aabfe03f4a93f66
Devon Kiss <tuffs@gmx.com>
2018-08-03 17:59:50 -0500

    Completed design with buttons, tooltips, icons, inputs, and checkboxes.

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Input, Button, Checkbox, Icon } from 'antd';
+import { Input, Button, Checkbox, Icon, Tooltip } from 'antd';
 
 class Todos extends Component {
 	constructor(props) {
@@ -19,24 +19,27 @@ class Todos extends Component {
 	};
 	handleFormSubmit = (e) => {
 		e.preventDefault();
-		const { todos, todo } = this.state;
-		const newTodo = {
-			value: todo,
-			completed: false
-		};
-		this.setState({
-			todos: todos.concat(newTodo),
-			todo: ''
-		});
+		if (this.state.todo !== '') {
+			const { todos, todo } = this.state;
+			const newTodo = {
+				value: todo,
+				completed: false
+			};
+			this.setState({
+				todos: todos.concat(newTodo),
+				todo: ''
+			});
+		}
 	};
 	handleTodoToggle = (index) => {
 		const completed = !this.state.todos[index].completed;
 		const newState = {
-			todo: '',
+			todo: this.state.todo,
 			todos: Object.assign(
 				[...this.state.todos],
 				{[index]: Object.assign({}, index, { value: this.state.todos[index].value, completed })}
-			)
+			),
+			error: this.state.error
 		}
 		this.setState(
 			newState
@@ -54,30 +57,35 @@ class Todos extends Component {
       	style={{marginLeft: '4px'}}
       >
         <h2>Todos List</h2>
+        {this.state.error}
         <div>
         	{this.state.todos.map((todo, i) => (
         			<li key={i}
         				style={{listStyle: 'none', paddingBottom: '25px'}}
         			>
-      					<Checkbox
-      						name="toggleComplete"
-      						type="checkbox"
-      						onChange={() => this.handleTodoToggle(i)}
-      						checked={todo.completed ? true : false}
-      						style={{marginRight: '5px'}}
-      					/>
-      					<span
-      						style={todo.completed ? { textDecoration: 'line-through' } : null }
-      						onClick={() => this.handleTodoToggle(i)}
-      					><strong>{todo.value}</strong></span>
-      					<Button
-      						type="primary"
-      						size="small"
-      						shape="circle"
-      						onClick={() => this.handleDeleteTodo(i)}
-      					>
-      						<Icon type="right-square-o" />
-      					</Button>
+        				<Tooltip title="Mark todo as complete">
+	      					<Checkbox
+	      						name="toggleComplete"
+	      						type="checkbox"
+	      						onChange={() => this.handleTodoToggle(i)}
+	      						checked={todo.completed ? true : false}
+	      						style={{marginRight: '5px'}}
+	      					/>
+	      					<span
+	      						style={todo.completed ? { textDecoration: 'line-through' } : null }
+	      						onClick={() => this.handleTodoToggle(i)}
+	      					><strong>{todo.value}</strong></span>
+      					</Tooltip>
+      					<Tooltip title="Delete Todo">
+	      					<Button
+	      					 	ghost
+	      						type="danger"
+	      						size="small"
+	      						onClick={() => this.handleDeleteTodo(i)}
+	      					>
+	      						<Icon type="delete" />
+	      					</Button>
+      					</Tooltip>
         			</li>
         	))}
         </div>
