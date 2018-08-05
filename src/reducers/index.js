edf54e9e47c96db40d5ece082575dbf651f4c4ac
Devon Kiss <tuffs@gmx.com>
2018-08-04 19:39:08 -0500

    Reducers split into todos and visibilityFilter scripts. Combined in reducers/index.js to provide initial state.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,33 +1,11 @@
 import { ADD_TODO, SET_VISIBILITY_FILTER } from "../actions";
-import { combineReducers } from 'redux';
+import { combineReducers } from "redux";
+import todos from "./todos";
+import visibilityFilter from "./visibilityFilter";
 
-function todoApp (state = initialState, action) {
-	switch (action.type) {
-		case SET_VISIBILITY_FILTER :
-			return Object.assign({}, state, {
-				visibilityFilter: action.filter
-			});
-		case ADD_TODO :
-			return Object.assign({}, state, {
-				todos: [
-					...state.todos,
-					{
-						value: action.value,
-						completed: false
-					}
-				]
-			});
-		default :
-			return state;
-  }
-	return state;
-}
-
-const initialState = {
-	visibilityFilter: 'SHOW_ALL',
-	todos: []
-};
-
-export default combineReducers({
-	todoApp
+const todoApp = combineReducers({
+  todos,
+  visibilityFilter
 });
+
+export default todoApp;
